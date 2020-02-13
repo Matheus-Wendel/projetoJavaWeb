005c4c4dcf7d6756b6d7a8742175be01ba846186
Matheus-Wendel <matheus_wendel@outlook.com>
2020-02-13 20:33:50 -0300

    commit

---
+++ src/test/java/com/fatec/javaweb/ProjetoJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.javaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
