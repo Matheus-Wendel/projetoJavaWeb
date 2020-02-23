5865ba92473ee8cd4b497c2dd7274e0062561114
Matheus-Wendel <matheus_wendel@outlook.com>
2020-02-23 16:48:40 -0300

    (DONE)Controller Para teste do Crawler

--- src/main/java/com/fatec/javaweb/controller/inicialController.java
+++ src/main/java/com/fatec/javaweb/controller/inicialController.java
@@ -1,18 +1,37 @@
 package com.fatec.javaweb.controller;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fatec.javaweb.model.ConjuntoServidores;
+import com.fatec.javaweb.service.CrawlerPortalTransparencia;
 
 @Controller
 @RequestMapping("")
 public class inicialController {
 	
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@GetMapping(value = "")
 	public ModelAndView inicial() {
 		ModelAndView mv = new ModelAndView("inicial/index");
 		return mv;
+	}
+	@GetMapping(value = "/nomeESalario")
+	@ResponseBody
+	public ConjuntoServidores TesteCrawler() {
+		CrawlerPortalTransparencia crawler = new CrawlerPortalTransparencia();
+		
+		try {
+			return crawler.getConjuntoServidores();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new ConjuntoServidores();
 	}
 
 }
