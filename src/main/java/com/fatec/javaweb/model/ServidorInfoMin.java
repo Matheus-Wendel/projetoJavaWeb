529748f89c9073306d792d727a26c181fcd0d659
Matheus-Wendel <matheus_wendel@outlook.com>
2020-02-23 16:43:41 -0300

    (DONE) Classes modelo usadas pelo Crawler

---
+++ src/main/java/com/fatec/javaweb/model/ServidorInfoMin.java
@@ -0,0 +1,61 @@
+package com.fatec.javaweb.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fatec.javaweb.desrializer.RendimentoDeserializer;
+
+public class ServidorInfoMin implements BaseModel {
+
+	
+
+	private String rgf;
+	private String nome;
+	private String cargo;
+	@JsonDeserialize(using = RendimentoDeserializer.class)
+	private Double rendimentos;
+
+	
+	
+	
+	public ServidorInfoMin() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public Double getRendimentos() {
+		return rendimentos;
+	}
+
+	public void setRendimentos(Double rendimento) {
+		this.rendimentos = rendimento;
+	}
+	
+
+	public String getRgf() {
+		return rgf;
+	}
+
+	public void setRgf(String rgf) {
+		this.rgf = rgf;
+	}
+
+	@Override
+	public String toString() {
+		return "ServidoresInfoMin [rgf=" + rgf + ", nome=" + nome + ", cargo=" + cargo + ", rendimentos=" + rendimentos + "]";
+	}
+
+	
+}
