1b725f45e72f15ac9fdd6e3d49f5e28794cb9a8f
Matheus-Wendel <matheus_wendel@outlook.com>
2020-03-01 01:40:39 -0300

    (DONE) classe abstrata para componentes do salario

---
+++ src/main/java/com/fatec/javaweb/model/Salario/ComponenteSalario.java
@@ -0,0 +1,48 @@
+package com.fatec.javaweb.model.Salario;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fatec.javaweb.deserializer.RendimentoDeserializer;
+import com.fatec.javaweb.model.BaseModel;
+
+@MappedSuperclass
+public abstract class ComponenteSalario implements BaseModel{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	@JsonDeserialize(using = RendimentoDeserializer.class)
+	private Double valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	
+	
+}
