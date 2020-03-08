34ab99dbe39a26d76d8293375a1951acb4517967
Matheus-Wendel <matheus_wendel@outlook.com>
2020-03-08 12:42:52 -0300

    (DONE) login com nivel de permissao

---
+++ src/main/java/com/fatec/javaweb/util/Util.java
@@ -0,0 +1,9 @@
+package com.fatec.javaweb.util;
+
+public class Util {
+
+	public static final String PAGINA_SUCESSO_LOGIN = "/administrativo";
+
+	public static final String PAGINA_SUCESSO_LOGOUT = "/";
+
+}
